fdc3b0519de87381a2ca38c5bb57589f31f240bb
RomanGranata <92897529+RomanGranata@users.noreply.github.com>
2022-03-04 15:17:50 -0300

    Actividad java

---
+++ ActividadObligatoriaJava.java
@@ -0,0 +1,33 @@
+package Ejemplos;
+
+import javax.swing.JOptionPane;
+
+public class ActividadObligatoriaJava {
+	
+	
+	public static void main(String[] args) {
+		
+		String nombre = JOptionPane.showInputDialog("Ingrese su Nombre");
+	    System.out.println(nombre);
+				
+		String apellido = JOptionPane.showInputDialog("Ingrese su Apellido");
+		System.out.println(apellido);
+		
+		String edad = JOptionPane.showInputDialog("ingrese su Edad");
+		System.out.println(edad);
+		
+		String hobbie = JOptionPane.showInputDialog("Ingrese su Hobbie");
+		System.out.println(hobbie);
+		
+		String editorcodigo = JOptionPane.showInputDialog("Ingrese su Editor de Codigo Preferido");
+	    System.out.println(editorcodigo);
+	    
+	    String sistemaoperativo = JOptionPane.showInputDialog("Ingrese su Sistema Operativo Preferido");
+	    System.out.println(sistemaoperativo);
+	    
+		
+		
+	}
+	
+	
+}
